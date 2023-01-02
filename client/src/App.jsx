ff41ac0476fd74edbc1ac78000ebc6c8a2ae473b
unknown <412863158@qq.com>
2023-01-02 14:48:49 +0800

    sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import { CampaignDetails,createCampaign,Home,Profile } from './pages'
+import { Navbar, Sidebar } from './components'
+
+
+
+
 
 function App() {
   return (
-    <div>App</div>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+    
+    <div className="sm:flex hidden mr-10 relative">
+      <Sidebar></Sidebar>
+    </div>
+
+    <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+      <Navbar></Navbar>
+
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </div>
+    </div>
   )
 }
 
